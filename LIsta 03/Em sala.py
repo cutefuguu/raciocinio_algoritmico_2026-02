2b2735552995af0aa5b086dd248861b6604a6d2e
cutefuguu <nakashima.taina2@gmail.com>
2026-08-27 09:20:16 -0300

    Create Em sala.py

---
+++ LIsta 03/Em sala.py
@@ -0,0 +1,25 @@
+'''
+# 1) 2,4,6,8...40
+numero = 0
+
+while numero <= 40:
+    print(numero)
+    numero += 2
+'''  
+'''
+
+# 2) -100,-90,-80...80,90,100
+numero = -100
+
+while numero <= 100:
+    print(numero)
+    numero += 10
+
+# 3) 7, 14, 21, 28...70
+numero = 7
+'''
+
+while numero <= 70:
+    print(numero)
+    numero += 7
+
